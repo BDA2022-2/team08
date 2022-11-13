5647a3a4638c5497a704058b33f0cd814ce229d4
mutjin <mutjin23@naver.com>
2022-11-14 01:38:06 +0900

    2022-11-14

---
+++ app/MyRecords_user.php
@@ -0,0 +1,52 @@
+<html>
+
+	<body>
+		<?php
+			$db_host="localhost";
+			$db_user="team08";
+			$db_password="team08";
+			$db_name="team08";
+
+			//connect to the database
+			$link=mysqli_connect($db_host,$db_user,$db_password,$db_name);
+			
+			//check connection
+			if($link===false){
+				die("error: could not connect".mysqli_connect_error());
+			}else{
+				//prepare an insert statement
+				//$sql="INSERT INTO `team08`.`mtn_review`(`review_id`,`mtn_idx`,`mtn_name`,`user_id`,`visit_date`,`mtn_rate`,`comment`,`created`) VALUES (?,?,?,?,?,?,?,?)";
+				//$sql="INSERT INTO `team08`.`mtn_review`(`mtn_idx`,`mtn_name`,`user_id`,`visit_date`,`mtn_rate`,`comment`,`created`) VALUES (?,?,?,?,?,?,?)";
+				$sql="select * from `team08`.`mtn_review`";
+				$res=mysqli_query($link,$sql);
+				
+				if($res){			
+					if($res){
+						while($newArray=mysqli_fetch_array($res,MYSQLI_ASSOC)){
+							$mtn_idx=$newArray['mtn_idx'];
+							$mtn_name=$newArray['mtn_name'];
+							$user_id=$newArray['user_id'];
+							$visit_date=$newArray['visit_date'];
+							$mtn_rate=$newArray['mtn_rate'];
+							$comment=$newArray['comment'];
+							$created=$newArray['created'];
+							
+							echo "	name={$mtn_name}";
+							echo "	comment={$comment}";
+							echo "	created={$created}";
+							echo "	<br>\n";
+							
+						}
+					}
+				}else{
+					echo "ERROR: Could not retrieve records: $sql".mysqli_error($link);
+				}
+			}
+			//close statement
+			mysqli_free_result($res);
+			//close connection
+			mysqli_close($link);
+		?>
+	</body>
+
+</html>
---
+++ app/deleteAccount_user.php
@@ -0,0 +1,54 @@
+<?php
+	$db_host="localhost";
+	$db_user="team08";
+	$db_password="team08";
+	$db_name="team08";
+
+    //connect to the database
+	$link=mysqli_connect($db_host,$db_user,$db_password,$db_name);
+	
+	//check connection
+	if($link===false){
+		die("error: could not connect".mysqli_connect_error());
+	}else{
+		//prepare an insert statement
+        $sql="DELETE FROM `team08`.`user` WHERE user_id=? and user_pw=? and user_name=?";
+		
+		if($stmt=mysqli_prepare($link,$sql)){
+			//bind variables to the prepared stmt as parameters
+			mysqli_stmt_bind_param($stmt,"sss",$user_id,$user_pw,$user_name);
+			
+			//set parameters
+			$user_id=trim($_POST['user_id']);
+			$user_pw=trim($_POST['user_pw']);
+			$user_name=trim($_POST['user_name']);
+
+			if (!$user_id||!$user_pw||!$user_name) {
+				echo "<script>alert('id, pw, name 모두 입력해 주세요');</script>";
+				echo "<script>location.replace('./deleteAccount.php');</script>";
+				exit;
+			}
+			
+			//attempt to execute the prepared statement
+			
+			if(mysqli_stmt_execute($stmt)&&mysqli_affected_rows($link)>0){
+				echo "<script>alert('Records deleted successfully');</script>";
+				echo "<script>location.replace('./deleteAccount.php');</script>";
+			}else if(mysqli_affected_rows($link)<1){
+				echo "<script>alert('ERROR: 잘못된 유저정보 입니다');</script>";
+				echo "<script>location.replace('./makeAccount.php');</script>";
+				exit;
+			}else{
+				echo "<script>alert('ERROR: Could not execute query');</script>";
+				echo "<script>location.replace('./deletemakeAccount.php');</script>";
+			}
+		}else{
+			echo "<script>alert('ERROR: Could not prepare query');</script>";
+			echo "<script>location.replace('./deletemakeAccount.php');</script>";
+		}
+	}
+	//close statement
+	mysqli_stmt_close($stmt);
+	//close connection
+    mysqli_close($link);
+?>
